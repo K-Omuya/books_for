a25a58824083ba7ccdc626724b13683564f6f107
omuya <simonotienoomuya@gmail.com>
2025-03-06 23:33:14 +0300

    Updated Books for All project files

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,7 +1,15 @@
+
 from django.contrib import admin
-from .models import BookClub
-from django.contrib import admin
-from .models import Impact
+from .models import Impact, BookClub, BookDonation,Book,BlogPost,PledgedBook,Testimonial,Message,Pledge
 
 admin.site.register(Impact)
 admin.site.register(BookClub)
+admin.site.register(BookDonation)
+admin.site.register(Book)
+admin.site.register(BlogPost)
+admin.site.register(PledgedBook)
+admin.site.register(Testimonial)
+admin.site.register(Message)
+admin.site.register(Pledge)
+
+
