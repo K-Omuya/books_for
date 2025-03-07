402c3f85c8813e171452a7ab6da17360723f0205
omuya <simonotienoomuya@gmail.com>
2025-03-07 13:31:54 +0300

    Updated Books for All project files

--- Books/settings.py
+++ Books/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'Books.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')),
 
 
 
