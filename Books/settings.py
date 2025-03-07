84daecfc880688c2131b7b86cd6d96ceab51d2ca
omuya <simonotienoomuya@gmail.com>
2025-03-07 13:24:10 +0300

    Updated Books for All project files

--- Books/settings.py
+++ Books/settings.py
@@ -81,15 +81,14 @@ WSGI_APPLICATION = 'Books.wsgi.application'
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://user:password@localhost/dbname')
-}
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+
+
+
+
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
 
 # Password validation
